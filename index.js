d77102b33829ff557ad5e9b415edc55a72da2834
Pankamol Srikaew <aim.ske09@gmail.com>
2017-08-05 18:53:49 +0700

    send gen file

--- index.js
+++ index.js
@@ -23,6 +23,25 @@ var template = {
 									"})"
 }
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyParser.json())
 
 /**
@@ -57,7 +76,7 @@ app.post('/generator', function (req, res) {
 		result += httpAction;
 	}
 	var toWriteFile = result;
-	fs.writeFile('/home/fptrainnie/StaticJavis/server.js', toWriteFile);
+	fs.writeFile('server.js', toWriteFile);
 	res.send([programmingLanguage, projectName, ""+result]);
 })
 
@@ -69,7 +88,7 @@ app.post('/generator', function (req, res) {
 **/
 app.get('/sentFile', function (req, res) {
 	console.log('GET - /sentFile')
-	var filePath =  "/home/fptrainnie/StaticJavis/server.js";
+	var filePath =  "server.js";
 	fs.exists(filePath, function(exists){
       if (exists) {     
         // Content-type is very interesting part that guarantee that
@@ -86,6 +105,58 @@ app.get('/sentFile', function (req, res) {
 	// res.send(programmingLanguage);
 })
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+/**
+* test server
+* Req: GET
+* params:              
+* return:
+**/
+app.post('/gen', function (req, res) {
+	console.log('POST - /gen')
+	var programmingLanguage = req.body.programmingLanguage;
+	var projectName = req.body.project;
+	var keywords = []
+
+
+	var result = template.dependencies;
+	for(var key in req.body.keywords){
+
+		var httpAction = "";
+
+		if(req.body.keywords[key].keyword == 'listen'){
+			httpAction = template.httpListen;
+		}
+		else{
+			httpAction = template.httpAction;
+		}
+
+		httpAction = template.comment.replace("<httpAction>", req.body.keywords[key].keyword) 
+			+ httpAction.replace(
+			/<httpAction>/g,req.body.keywords[key].keyword).replace(
+			/<param>/g, req.body.keywords[key].param)
+		result += httpAction;
+	}
+	var toWriteFile = result;
+	fs.writeFile('server.js', toWriteFile);
+
+	var filePath =  "server.js";
+	fs.exists(filePath, function(exists){
+      if (exists) {     
+        // Content-type is very interesting part that guarantee that
+        // Web browser will handle response in an appropriate manner.
+        res.writeHead(200, {
+          "Content-Type": "application/octet-stream",
+          "Content-Disposition" : "attachment; filename=server.js"});
+        fs.createReadStream(filePath).pipe(res);
+      } else {
+        res.writeHead(400, {"Content-Type": "text/plain"});
+        res.end("ERROR File does NOT Exists");
+      }
+    });
+})
+
+
+
+app.listen(3001, function () {
+  console.log('App listening on port 3001!')
 })
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+
+/**
+* Description: 
+* Req: get
+* Params:
+* Return:
+**/
+app.get('/test', function (req, res) {
+  console.log('get – /test')
+  res.send('get method!');
+})
+
+/**
+* Description: 
+* Req: post
+* Params:
+* Return:
+**/
+app.post('/lol', function (req, res) {
+  console.log('post – /lol')
+  res.send('post method!');
+})
+
+/**
+* Description: 
+* Req: listen
+* Params:
+* Return:
+**/
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})

4ae372dea60a45958c0e951ec0090bf200f3e70c
Pankamol Srikaew <aim.ske09@gmail.com>
2017-08-05 14:09:50 +0700

    finally we can install it lol

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const app = express()
 
 app.use(bodyParser.json())
 
 /**
 * test server
 * Req: GET
-* params:
+* params:              
 * return:
 **/
-app.get('/', function (req, res) {
-	console.log('GET - /')
-	res.send('Hello World!')
+app.post('/generator', function (req, res) {
+	console.log('POST - /generator')
+	
+	var programmingLanguage = req.body.programmingLanguage;
+	console.log(programmingLanguage)
+	res.send(programmingLanguage);
 })
 
 app.listen(3000, function () {
